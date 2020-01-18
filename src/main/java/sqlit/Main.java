e2f0764a4076b0d845bbc898ba57df986eee5ccc
Garrett Brenner <garrebrenna@gmail.com>
2020-01-18 18:47:23 -0500

    init commit

---
+++ src/main/java/sqlit/Main.java
@@ -0,0 +1,15 @@
+package sqlit;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("SQLit");
+        
+    }
+}
