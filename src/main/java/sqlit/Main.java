e8bc5aef8a818905028958c20b3faae7ab6950b3
Garrett Brenner <garrebrenna@gmail.com>
2020-01-19 13:38:58 -0500

    loaded logo

--- src/main/java/sqlit/Main.java
+++ src/main/java/sqlit/Main.java
@@ -5,8 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.io.FileNotFoundException;
 
 public class Main extends Application {
 
@@ -16,21 +19,38 @@ public class Main extends Application {
 
     private static final String SQLITE_LOGO = "sqlit/logo-01.png";
 
+    //**********************************************//
+    //                  ATTRIBUTES                  //
+    //**********************************************//
+
+    private static Image logo;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("SQLit");
 
         Parent root = FXMLLoader.load(getClass().getResource("main-window.fxml"));
 
-        //set Stage boundaries to visible bounds of the main screen
+        // Set Stage boundaries to visible bounds of the main screen
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         primaryStage.setX(primaryScreenBounds.getMinX());
         primaryStage.setY(primaryScreenBounds.getMinY());
         primaryStage.setWidth(primaryScreenBounds.getWidth());
         primaryStage.setHeight(primaryScreenBounds.getHeight());
 
+        // Load logo
+        try {
+            primaryStage.getIcons().add(new Image(SQLITE_LOGO));
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static Image getLogo() {
+        return logo;
     }
 
     public static void main(String[] args) {
