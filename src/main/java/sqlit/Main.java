a391b76cde4b20fc57a01e03ad304306171330a1
Garrett Brenner <garrebrenna@gmail.com>
2020-01-19 12:55:48 -0500

    working window

--- src/main/java/sqlit/Main.java
+++ src/main/java/sqlit/Main.java
@@ -2,14 +2,38 @@ package sqlit;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    //**********************************************//
+    //                  CONSTANTS                   //
+    //**********************************************//
+
+    private static final String SQLITE_LOGO = "sqlit/logo-01.png";
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("SQLit");
-        
+
+        Parent root = FXMLLoader.load(getClass().getResource("main-window.fxml"));
+
+        //set Stage boundaries to visible bounds of the main screen
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX(primaryScreenBounds.getMinX());
+        primaryStage.setY(primaryScreenBounds.getMinY());
+        primaryStage.setWidth(primaryScreenBounds.getWidth());
+        primaryStage.setHeight(primaryScreenBounds.getHeight());
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/main/java/sqlit/MainWindow.java
@@ -0,0 +1,6 @@
+package sqlit;
+
+import javafx.scene.layout.AnchorPane;
+
+public class MainWindow extends AnchorPane {
+}
